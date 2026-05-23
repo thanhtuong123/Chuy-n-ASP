669396f7eabeb351c45bebd56cea30381e066124
Lethanhtuong <trangngocmau26121982@gmail.com>
2026-05-23 11:24:00 +0700

    Hoan thanh buoi 2 ASP.NET Core EF

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace CMS.Data.Entities
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục)
+		public string Description { get; set; }
+
+		// Quan hệ: Một danh mục có nhiều bài viết
+		public virtual ICollection<Post> Posts { get; set; }
+	}
+}
