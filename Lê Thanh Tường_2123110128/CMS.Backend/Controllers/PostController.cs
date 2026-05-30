0a133310f33c609b00e4801db85d84c211fd3f2a
Lethanhtuong <trangngocmau26121982@gmail.com>
2026-05-30 10:38:57 +0700

    Hoan thanh buoi 3 LINQ CRUD

---
+++ Lê Thanh Tường_2123110128/CMS.Backend/Controllers/PostController.cs
@@ -0,0 +1,157 @@
+﻿using CMS.Data;
+using CMS.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Backend.Controllers
+{
+	[Authorize]
+	public class PostController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public PostController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index(int? id)
+		{
+			var posts = _context.Posts
+				.Include(p => p.Category)
+				.AsQueryable();
+
+			if (id != null)
+			{
+				posts = posts.Where(p => p.CategoryId == id);
+			}
+
+			var data = posts
+				.OrderByDescending(p => p.CreatedDate)
+				.ToList();
+
+			return View(data);
+		}
+
+		public IActionResult Details(int id)
+		{
+			var post = _context.Posts
+				.Include(p => p.Category)
+				.FirstOrDefault(p => p.Id == id);
+
+			if (post == null)
+			{
+				return NotFound();
+			}
+
+			return View(post);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			ViewBag.CategoryList = new SelectList(_context.Categories, "Id", "Name");
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Post model, IFormFile? uploadImage)
+		{
+			if (uploadImage != null && uploadImage.Length > 0)
+			{
+				string folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+				if (!Directory.Exists(folder))
+				{
+					Directory.CreateDirectory(folder);
+				}
+
+				string fileName = Guid.NewGuid().ToString() + Path.GetExtension(uploadImage.FileName);
+				string filePath = Path.Combine(folder, fileName);
+
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					uploadImage.CopyTo(stream);
+				}
+
+				model.ImageUrl = "/uploads/" + fileName;
+			}
+
+			model.CreatedDate = DateTime.Now;
+
+			_context.Posts.Add(model);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			var post = _context.Posts.Find(id);
+
+			if (post == null)
+			{
+				return NotFound();
+			}
+
+			ViewBag.CategoryList = new SelectList(_context.Categories, "Id", "Name", post.CategoryId);
+
+			return View(post);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Post model, IFormFile? uploadImage)
+		{
+			var post = _context.Posts.Find(model.Id);
+
+			if (post == null)
+			{
+				return NotFound();
+			}
+
+			post.Title = model.Title;
+			post.Content = model.Content;
+			post.CategoryId = model.CategoryId;
+
+			if (uploadImage != null && uploadImage.Length > 0)
+			{
+				string folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+				if (!Directory.Exists(folder))
+				{
+					Directory.CreateDirectory(folder);
+				}
+
+				string fileName = Guid.NewGuid().ToString() + Path.GetExtension(uploadImage.FileName);
+				string filePath = Path.Combine(folder, fileName);
+
+				using (var stream = new FileStream(filePath, FileMode.Create))
+				{
+					uploadImage.CopyTo(stream);
+				}
+
+				post.ImageUrl = "/uploads/" + fileName;
+			}
+
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var post = _context.Posts.Find(id);
+
+			if (post != null)
+			{
+				_context.Posts.Remove(post);
+				_context.SaveChanges();
+			}
+
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Lê Thanh Tường_2123110128/CMS.Backend/Views/Category/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMS.Backend.Views.Category
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
